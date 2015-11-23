795d27f34336800a714f63b5cf08dbdf365fa106
sergey <sergey@cheparev.com>
2015-11-23 16:39:35 +0100

    Relayr mqtt stream start

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+import json
 from kivy.app import App
 
 
 class RelayrSensorApp(App):
-    pass
+    def __init__(self, **kwargs):
+        super(RelayrSensorApp, self).__init__(**kwargs)
+
+        with open("relayr_credentials.json", "r") as f:
+            self.relayr_config = json.load(f)
+        self.relayr_configure()
+
+    def mqtt_callback(self, topic, payload):
+        print topic, payload
+
+    def relayr_configure(self):
+        from relayr import Client
+        from relayr.dataconnection import MqttStream
+        self.client = Client(token=self.relayr_config["token"])
+        self.device = self.client.get_device(id=self.relayr_config["devices"][0]["id"])
+        self.mqtt_stream = MqttStream(self.mqtt_callback, [self.device])
+
+    def on_start(self):
+        self.mqtt_stream.start()
+
+    def on_stop(self):
+        self.mqtt_stream.stop()
+
+    def build(self):
+        return
 
 
 from kivy.core.window import Window
