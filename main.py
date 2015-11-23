d02c63d94271d167ba9207410bfd8847e58b9f39
sergey <sergey@cheparev.com>
2015-11-23 15:41:38 +0100

    App sketch

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+
+
+class RelayrSensorApp(App):
+    pass
+
+
+from kivy.core.window import Window
+
+Window.clearcolor = (1, 1, 1, 1)
+
+app = RelayrSensorApp()
+app.run()
